4a017b40c9ae93fbb0e584a4d071443d742fec92
Sultonkhon Oblokulov <sultondeveloper@gmail.com>
2022-08-13 18:47:17 +0500

    done: header and menu of site"

---
+++ src/components/Header/Header.component.tsx
@@ -0,0 +1,73 @@
+import { useRecoilState } from "recoil";
+import { navBarDefaultStateData } from "../../recoil/atoms.state";
+import NavBar from "../NavBar/NavBar.component";
+import NavBarMob from "../NavBar/NavBarMob.component";
+import ThemeChanger from "../ThemeChanger/ThemeChanger.component";
+
+const Header = () => {
+  const [menuState, setMenuState] = useRecoilState<string>(
+    navBarDefaultStateData
+  );
+
+  function setClassState(
+    state: string,
+    stateChanger: (value: string) => void,
+    firstClass: string,
+    secondClass: string
+  ) {
+    if (state === firstClass) {
+      stateChanger(secondClass);
+    } else {
+      stateChanger(firstClass);
+    }
+  }
+
+  return (
+    <header className="fixed top-0 w-full bg-gray-600/5 dark:bg-gray-600/5 backdrop-blur-md py-4">
+      <div
+        className="container flex justify-between items-center mx-auto 
+        ex-sm:px-4
+        sm:px-2
+        md:px-0
+      "
+      >
+        <h1 className="text-2xl dark:text-white ex-sm:font-bold">
+          &#60;<span className="md:inline ex-sm:hidden">SULTON</span>DEV/&#62;
+        </h1>
+        <NavBar />
+        <NavBarMob />
+
+        <div className="md:hidden ex-sm:flex justify-center items-center">
+          <button
+            className="text-4xl text-black dark:text-blue-600"
+            onClick={() => {
+              setClassState(
+                menuState,
+                setMenuState,
+                "animate-show-menu left-0",
+                "animate-hide-menu -left-[100%]"
+              );
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 text-inherit"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 8h16M4 16h16"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/NavBar/NavBarMob.component.tsx
@@ -0,0 +1,75 @@
+import { useRecoilState } from "recoil";
+import { navBarDefaultStateData } from "../../recoil/atoms.state";
+
+import ThemeChanger from "../ThemeChanger/ThemeChanger.component";
+const NavBarMob = () => {
+  const [menuState, setMenuState] = useRecoilState<string>(
+    navBarDefaultStateData
+  );
+
+  return (
+    <nav
+      className={`nav fixed w-full
+    h-full
+    md:hidden
+    top-0
+    z-10
+    ${menuState}
+    `}
+    >
+      <div
+        className="flex flex-col 
+        justify-between
+        items-start
+        h-screen
+        bg-[#243b4a]
+        dark:bg-[#111a20]
+        p-4
+        "
+      >
+        <button
+          className="ex-sm:text-2xl text-left text-white dark:text-blue-600"
+          onClick={() => {
+            setMenuState("animate-hide-menu -left-[100%]");
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8 text-inherit"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </button>
+        <ul
+          className="
+        flex flex-col
+        text-black
+        font-bold
+        text-2xl   
+        dark:text-white
+        ex-sm:gap-4
+        "
+        >
+          <li>Home</li>
+          <li>Skills</li>
+          <li>Projects</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        <div>
+          <ThemeChanger />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBarMob;
